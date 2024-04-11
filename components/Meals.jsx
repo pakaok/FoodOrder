5a9e2fa24e18cf19c778987c4ba5e71ba14b80cb
pakaok <109903846+pakaok@users.noreply.github.com>
2024-04-11 17:38:59 +0900

    Feat: Add MealItem - fetch data from backend

--- components/Meals.jsx
+++ components/Meals.jsx
@@ -1,13 +1,27 @@
 import { useState } from "react";
 import { useEffect } from "react";
+import MealItem from "./MealItem";
+
+// "id","name" "price" "description" "image"
 
 export default function Meals() {
-  const [meals, setMeals] = useState([]);
+  const [loadedMeals, setLoadedMeals] = useState([]);
+  
   useEffect(() => {
-    fetch("http://localhost:3000/meals")
-      .then((res) => res.json())
-      .then(res=>console.log(res));
+    async function fetchMeals(){
+        try{
+            const response = await fetch('http://localhost:3000/meals')
+            if(!response.ok){
+                console.log('Error')
+            }
+            const meals = await response.json()
+            setLoadedMeals(meals)
+        }catch(err){
+            console.log(err.message)
+        }
+      }
+    fetchMeals()
   },[]);
 
-  return <ul id="meals"></ul>;
+  return <ul id="meals">{loadedMeals.map(loadedMeal => < MealItem key={loadedMeal.id} meal={loadedMeal}/>)}</ul>;
 }
