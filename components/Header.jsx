d27c72dba392a2c8dce6f9db6b07938c84c2d091
pakaok <109903846+pakaok@users.noreply.github.com>
2024-04-12 16:27:52 +0900

    Feat: Add Modal Component

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
 import logoImg from '../src/assets/logo.jpg'
 import Button from './UI/Button';
+import { CartContext } from '../store/CartContext';
 export default function Header() {
+  const cartCtx = useContext(CartContext)
+  const totalCartItems = cartCtx.items.reduce((prevValue,CurrentItem)=> prevValue + CurrentItem.quantity,0)
   return (
     <header id="main-header">
       <div id="title">
@@ -8,7 +12,7 @@ export default function Header() {
         <h1>React Food</h1>
       </div>
       <nav>
-        <Button textOnly>Cart</Button>
+        <Button textOnly>Cart ({totalCartItems}) </Button>
       </nav>
     </header>
   );
