574c0a8679199a1d082d08f210bd781059cfd0ea
pakaok <109903846+pakaok@users.noreply.github.com>
2024-04-14 10:12:50 +0900

    Feat: Fetch Data in Checkout.jsx

--- components/UI/Checkout.jsx
+++ components/UI/Checkout.jsx
@@ -16,16 +16,26 @@ export default function Checkout() {
   function handleClose() {
     userProgressCtx.hideCheckout();
   }
+  function handleSubmit(event) {
+    event.preventDefault();
+    const fd = new FormData(event.target);
+    const customerData = Object.fromEntries(fd.entries());
+    fetch("http://localhost:3000/orders", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ order: {items:cartCtx.items, customer: customerData} }),
+    });
+  }
   return (
     <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotalPrice)} </p>
-        <Input label="Full Name" type="text" id="full-name" />
+        <Input label="Full Name" type="text" id="name" />
         <Input label="E-mail Address" type="email" id="email" />
         <Input label="Street" type="text" id="street" />
         <div className="control-row">
-          <Input label="Postal Code" type="text" id="Postal Code" />
+          <Input label="Postal Code" type="text" id="postal-code" />
           <Input label="City" type="text" id="city" />
         </div>
         <p className="modal-actions">
