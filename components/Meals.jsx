67cb48fbbfb94b13bca06b18f0cbbed9921f6d6e
pakaok <109903846+pakaok@users.noreply.github.com>
2024-04-16 15:37:01 +0900

    Feat: Error Handling in Meals.jsx

--- components/Meals.jsx
+++ components/Meals.jsx
@@ -2,14 +2,17 @@ import { useState } from "react";
 import { useEffect } from "react";
 import MealItem from "./MealItem";
 import useHttp from "../hooks/useHttp";
+import Error from "./Error";
 // "id","name" "price" "description" "image"
 const requestConfig = {}
 export default function Meals() {
-  const {data:loadedMeals,isLoading} = useHttp('http://localhost:3000/meals',requestConfig,[])
+  const {data:loadedMeals,isLoading,error} = useHttp('http://localhost:3000/meals',requestConfig,[])
   
   if (isLoading){
-    return <p>Fetching Data...</p>
+    return <p className="center">Fetching Data...</p>
   }
-
+  if(error){
+    return <Error title='Failed to fetch data' message={error} />
+  }
   return <ul id="meals">{loadedMeals.map(loadedMeal => < MealItem key={loadedMeal.id} meal={loadedMeal}/>)}</ul>;
 }
